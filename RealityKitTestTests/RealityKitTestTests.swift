81c422a2f92c8a062be65b399f059f551710ac2b
Konrad Feiler <konrad@mathheartcode.com>
2025-09-24 11:26:17 +0200

    Initial Commit

---
+++ RealityKitTestTests/RealityKitTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  RealityKitTestTests.swift
+//  RealityKitTestTests
+//
+//  Created by Konrad Feiler on 24.09.25.
+//
+
+import Testing
+@testable import RealityKitTest
+
+struct RealityKitTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
